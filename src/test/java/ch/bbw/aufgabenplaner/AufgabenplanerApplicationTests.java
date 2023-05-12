8e841491039b34087371c21ecdd4393e0b8ec8f0
esilva <esilva@ethz.ch>
2023-05-12 09:04:30 +0200

    Files

---
+++ src/test/java/ch/bbw/aufgabenplaner/AufgabenplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.aufgabenplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AufgabenplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
