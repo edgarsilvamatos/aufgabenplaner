5e373ea4374f61c3df8ee04dbf115526f85abf32
esilva <esilva@ethz.ch>
2023-06-30 07:53:07 +0200

    errors

--- src/main/java/ch/bbw/aufgabenplaner/AufgabeController.java
+++ src/main/java/ch/bbw/aufgabenplaner/AufgabeController.java
@@ -1,5 +1,6 @@
 package ch.bbw.aufgabenplaner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import java.util.ArrayList;
 @Controller
 public class AufgabeController {
 
+    @Autowired
+    private AufgabeRepository aufgabeRepository;
     private ArrayList<Aufgabe> aufgaben = new ArrayList<>();
 
     @GetMapping("/")
---
+++ src/main/java/ch/bbw/aufgabenplaner/AufgabeRepository.java
@@ -0,0 +1,9 @@
+package ch.bbw.aufgabenplaner;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+
+public interface AufgabeRepository extends CrudRepository<Aufgabe, Long> {
+    List<Aufgabe> findByLastname(String lastname);
+}
